4098b9f8173fad5462968c66466bf2a7c2a4556d
ankurbit1997 <31044900+ankurbit1997@users.noreply.github.com>
2021-10-25 16:45:58 +0530

    show no products if cart empty

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -11,23 +11,30 @@ const Cart = () => {
 
   return (
     <div className="cart">
-      <div className="cart__list">
-        <div className="cart__list__label">
-          <h3>Product</h3>
-          <h3>Price</h3>
-          <h3>Qty</h3>
-          <h3>Total</h3>
-        </div>
-
-        {cart.map((product, i) => (
-          <div className="cart__list__products" key={i}>
-            <img src={product.image} alt={product.title} />
-            <p>${product.price}</p>
-            <p>{product.qty || 1}</p>
-            <p>${product.price}</p>
+      {cart.length === 0 ? (
+        <p style={{ textAlign: "center", marginTop: "4rem", flex: "3" }}>
+          No products
+        </p>
+      ) : (
+        <div className="cart__list">
+          <div className="cart__list__label">
+            <h3>Product</h3>
+            <h3>Price</h3>
+            <h3>Qty</h3>
+            <h3>Total</h3>
           </div>
-        ))}
-      </div>
+
+          {cart.map((product, i) => (
+            <div className="cart__list__products" key={i}>
+              <img src={product.image} alt={product.title} />
+              <p>${product.price}</p>
+              <p>{product.qty || 1}</p>
+              <p>${product.price}</p>
+            </div>
+          ))}
+        </div>
+      )}
+
       <div className="cart__total">total</div>
     </div>
   );
